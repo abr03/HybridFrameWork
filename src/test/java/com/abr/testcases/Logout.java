9ba8cf0e5d3a4122c6035bdb06ceb2ea6875e996
abr03 <33234889+abr03@users.noreply.github.com>
2024-08-05 18:12:36 +0530

    added test cases for logout

--- src/test/java/com/abr/testcases/Logout.java
+++ src/test/java/com/abr/testcases/Logout.java
@@ -1,5 +1,17 @@
 package com.abr.testcases;
 
+import org.testng.annotations.Test;
+
 public class Logout {
+	
+	@Test
+	public void checklogout() {
+		
+	}
+	
+	@Test
+	public void doLogout() {
+		
+	}
 
 }

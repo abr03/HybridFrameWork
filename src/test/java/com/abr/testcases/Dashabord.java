4ed0be6fc2a745656f0697be9527ace42a705d95
abr03 <33234889+abr03@users.noreply.github.com>
2024-08-09 09:09:28 +0530

    Added Testcase for Dashboard branch Akula Bhaskar

--- src/test/java/com/abr/testcases/Dashabord.java
+++ src/test/java/com/abr/testcases/Dashabord.java
@@ -1,5 +1,12 @@
 package com.abr.testcases;
 
+import org.testng.annotations.Test;
+
 public class Dashabord {
 
+	
+	@Test
+	public void TestDashabord() {
+		System.out.println("This is Dashabord");
+	}
 }
